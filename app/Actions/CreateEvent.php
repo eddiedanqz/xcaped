91b58adb15d675421d977896b6e0bcadf0cdc229
Edmund Danquah <67321809+eddiedanqz@users.noreply.github.com>
2024-06-08 14:59:26 +0000

    added transaction

--- app/Actions/CreateEvent.php
+++ app/Actions/CreateEvent.php
@@ -3,35 +3,38 @@
 namespace App\Actions;
 
 use App\Notifications\EventPublished;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Notification;
 
 class CreateEvent
 {
     public function execute($data, $request)
     {
-        $event = auth()->user()->events()->create($data);
+        DB::transaction(function ($data, $request) {
+            $event = auth()->user()->events()->create($data);
 
-        if ($request->hasFile('image')) {
-            $event
-                ->getFirstMedia('banner')
-                ?->delete();
+            if ($request->hasFile('image')) {
+                $event
+                    ->getFirstMedia('banner')
+                    ?->delete();
 
-            $event->addMedia($data['image'])
-                ->toMediaCollection('banner');
-        }
+                $event->addMedia($data['image'])
+                    ->toMediaCollection('banner');
+            }
 
-        if ($request->has('tickets')) {
-            $event->ticket()->createMany($request->tickets);
-        }
+            if ($request->has('tickets')) {
+                $event->ticket()->createMany($request->tickets);
+            }
 
-        $details = [
-            'title' => $event->id,
-            'body' => auth()->user()->username.' created a new event',
-        ];
+            $details = [
+                'title' => $event->id,
+                'body' => auth()->user()->username.' created a new event',
+            ];
 
-        //Send notification
-        $users = auth()->user()->profile->followers;
-        Notification::send($users, new EventPublished($details));
+            //Send notification
+            $users = auth()->user()->profile->followers;
+            Notification::send($users, new EventPublished($details));
+        });
 
     }
 }
